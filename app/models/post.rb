eec788bad51f4a6e4affec0a5807ed3b4523c526
Mike Loseto <losetom1998@gmail.com>
2022-11-01 17:13:35 -0400

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+    # the title cannot be blank
+    validates :title, presence: true
+
+    #the category is at least 100 characters long 
+    validates :content, length: { minimum: 100 }
+
+    # the category is either Fiction or NonFiction
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
 end
